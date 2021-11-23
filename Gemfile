1321d3829fa9a6f84ec66392efaa3c08777505a4
ricky lin <ricky@bitoex.com>
2021-11-23 15:51:20 +0800

    add gem annotate

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'local_time'
 gem 'carrierwave'
+gem 'annotate', '~> 3.1', '>= 3.1.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- test/models/resume_test.rb
+++ test/models/resume_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: resumes
+#
+#  id         :integer          not null, primary key
+#  job_id     :integer
+#  user_id    :integer
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  attachment :string
+#
 require 'test_helper'
 
 class ResumeTest < ActiveSupport::TestCase
